aa638959dbb3b3575cd4d3f0818ab223115e7549
Florian-ROUSSEAU <80812610+Florian-ROUSSEAU@users.noreply.github.com>
2021-03-24 11:37:05 +0100

    DAO

---
+++ src/DAO/ConnexionBdd.java
@@ -0,0 +1,86 @@
+/*
+ * Classe de connexion à la base de données mysql Imagin
+ * 20/01/2021
+ * author : Zakina
+ */
+package DAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ *
+ * @author Zakina
+ */
+public class ConnexionBdd {
+
+public static Connection connection=null;
+public static Statement st=null;
+public static ResultSet rs=null;
+
+
+ // Méthode de création et d'ouverture de la connexion
+public static Connection ouvrirConnexion(){
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+    }
+    
+    try {
+    //obtention de la connexion
+        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/imagin","root","");
+        System.out.println("Connexion OK");
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return connection ;
+
+}
+
+ // Méthode de fermeture du resultset
+ public static void fermerConnexion(ResultSet rs)
+ {
+    if(rs!=null){
+        try{
+            rs.close();
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("Erreur lors de la fermeture d’une connexion dans fermerConnexion(ResultSet)");
+        }
+    }
+}
+ 
+ // Méthode de fermeture du statement
+public static void fermerConnexion(Statement stmt)
+{
+    if(stmt!=null){
+        try{
+            stmt.close();
+        }
+            catch(Exception e)
+        {
+            System.out.println("Erreur lors de la fermeture d’une connexion dans fermerConnexion(Statement)");
+        }
+    }
+}
+
+
+/// Méthode de fermeture de la connexion
+public static void fermerConnexion(Connection con)
+{
+    if(con!=null){
+        try{
+            con.close();
+            System.out.println("Fermeture Connexion OK");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Erreur lors de la fermeture d’une connexion dans fermerConnexion(Connection)");
+        }
+    }
+}
+}
---
+++ src/DAO/EnginTypeDAO.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import modele.EnginType;
+
+/**
+ *
+ * @author ts1sio
+ */
+public class EnginTypeDAO {
+     Connection connection=null;
+    static PreparedStatement requete = null;
+    static ResultSet rs = null;
+    
+
+        public static EnginType getEnginById(Connection connection, int matricule){
+            EnginType unEnginType = new EnginType();
+            try {
+                requete=connection.prepareStatement("SELECT * FROM ENFINTYPE WHERE ENGTYP_ID = ?");
+                requete.setString(1, String.valueOf(matricule));
+
+                //executer la reguete
+                rs=requete.executeQuery();
+                if (rs.next()){
+                    unEnginType.setId(Integer.parseInt(rs.getString("ENGTYP_ID")));
+                    unEnginType.setLibelle(rs.getString("ENGTYP_LIBELLE"));
+                  
+                }
+                    ConnexionBdd.fermerConnexion(rs);
+                    ConnexionBdd.fermerConnexion(requete); 
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+
+            return unEnginType; 
+        }
+        
+        
+        
+        
+        public static int AddTypeEngin(Connection connection, String libelle){
+            int resultatUpdate = -1;
+            try {
+                requete = connection.prepareStatement("INSERT INTO ENGINTYPE(ENGTYP_LIBELLE) VALUES (?)");
+                
+                requete.setString(1, libelle);
+
+                
+                resultatUpdate = requete.executeUpdate();
+                
+                ConnexionBdd.fermerConnexion(rs);
+                ConnexionBdd.fermerConnexion(requete);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultatUpdate;
+        }
+        
+    
+        
+        
+        
+        
+        
+        public static int RemoveTypeEngin(Connection connection, int pPode){
+            int resultatUpdate = -1;
+            try {
+                requete = connection.prepareStatement("DELETE FROM ENGINTYPE WHERE ENGTYP_ID = ?");
+                
+                requete.setString(1, String.valueOf(pPode));
+
+                
+                resultatUpdate = requete.executeUpdate();
+                
+                ConnexionBdd.fermerConnexion(rs);
+                ConnexionBdd.fermerConnexion(requete);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultatUpdate;
+        }
+        
+        
+        
+        
+}
